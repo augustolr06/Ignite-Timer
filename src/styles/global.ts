8724f849f51953c1ce6eaac7931ec7d6a912e8eb
Augusto Lázaro <augusto.lr06@gmail.com>
2023-01-18 11:26:40 -0300

    feat: add global.ts

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  } 
+`
+
