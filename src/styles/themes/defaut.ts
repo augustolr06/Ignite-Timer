74da2c831b00a172dc79401f1b7028ee041803b4
Augusto Lázaro <augusto.lr06@gmail.com>
2023-01-18 11:39:48 -0300

    feat: add theme colors that match the design

--- src/styles/themes/defaut.ts
+++ src/styles/themes/defaut.ts
@@ -1,9 +1,25 @@
 import { AppTheme } from "../../@types/styled";
 
 export const defaultTheme: AppTheme = {
-  primary: '#5726c9',
-  secondary: '#d37c35',
-  neutral: '#747474'
+  white: '#FFF',
+
+  gray_100: '#E1E1E6',
+  gray_300: '#C4C4CC',
+  gray_400: '#8D8D99',
+  gray_500: '#7C7C8A',
+  gray_600: '#323238',
+  gray_700: '#29292E',
+  gray_800: '#202024',
+  gray_900: '#121214',
+
+  green_300: '#00B37E',
+  green_500: '#00875F',
+  green_700: '#015F43',
+
+  red_500: '#AB222E',
+  red_700: '#7A1921',
+
+  yellow_500: '#FBA94C',
 }
 
 
