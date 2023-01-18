c3f07206d3309eb9d3543e3c24a6ef3707d58dbb
Augusto Lázaro <augusto.lr06@gmail.com>
2023-01-18 11:54:43 -0300

    fix: add theme color to global styles and focus

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,8 +8,13 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background-color: #333;
-    color: #fff;
+    background-color: ${props => props.theme.gray_900};
+    color: ${props => props.theme.gray_300};
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme.green_500};
   }
 
   body, input, textarea, button {
