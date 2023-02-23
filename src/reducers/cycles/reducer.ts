b4126815bb11344216c31b04d27452d9506186e9
Augusto Lázaro <augusto.lr06@gmail.com>
2023-02-23 10:42:14 -0300

    feat: apply Immer on cycles info updating

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer'
+
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -13,27 +15,27 @@ interface CyclesState {
   cycles: Cycle[]
   activeCycleId: string | null
 }
-
 export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      }
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) return state // devolve para o case o estado como está pois não existe um ciclo ativo (embora esse case não vá ser chamado sem que haja um ciclo ativo).
+
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].interruptedDate = new Date()
+        draft.activeCycleId = null
+      })
+    }
+
     case ActionTypes.INTERRUPT_CURRENT_CYCLE:
       return {
         ...state,
